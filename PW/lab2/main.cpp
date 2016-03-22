91b1065e13d82f206cd33c79e489ad62ddde4aad
vitiok <lucavictor220@gmail.com>
2016-03-22 20:44:53 +0200

    Added listBox and add button with control

--- PW/lab2/main.cpp
+++ PW/lab2/main.cpp
@@ -1,13 +1,22 @@
 #include <windows.h>
 #include "resource.h"
 
+HWND hListBox, hAddButton;
+HMENU hmenu;
+HWND hnewItem;
+
+
 LRESULT CALLBACK WndProc (HWND, UINT, WPARAM, LPARAM) ;
+
+void createWindows(HWND);
 
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
     static TCHAR szAppName[] = TEXT ("HelloWin") ;
     HWND hwnd ;
     MSG msg ;
     WNDCLASS wndclass ;
+
+
     wndclass.style = CS_HREDRAW | CS_VREDRAW ;
     wndclass.lpfnWndProc = WndProc ;
     wndclass.cbClsExtra = 0 ;
@@ -18,10 +27,12 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine
     wndclass.hbrBackground = (HBRUSH) GetStockObject (WHITE_BRUSH) ;
     wndclass.lpszMenuName = NULL ;
     wndclass.lpszClassName = szAppName ;
+
     if (!RegisterClass (&wndclass)) {
         MessageBox (NULL, TEXT ("This program requires Windows NT!"), szAppName, MB_ICONERROR) ;
         return 0 ;
     }
+
     hwnd = CreateWindow (szAppName, // window class name
                          TEXT ("Lab 2 "), // window caption
                          WS_OVERLAPPEDWINDOW, // window style
@@ -45,12 +56,32 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
     HDC hdc ;
     PAINTSTRUCT ps ;
     RECT rect ;
+    UINT iTextLength;
+    char* szText;
+
+
     switch (message) {
+        case WM_CREATE:
+            hmenu = CreateMenu();
+            AppendMenu(hmenu, MF_STRING, ID_ABOUT, "About");
+            AppendMenu(hmenu, MF_STRING, ID_EXIT, "Exit");
+            SetMenu(hwnd, hmenu);
+            createWindows(hwnd);
+            break;
+        case WM_COMMAND:
+            switch (LOWORD(wParam)){
+                case ID_ADD_BUTTON:
+                    iTextLength = SendMessage(hnewItem, WM_GETTEXTLENGTH, 0, 0);
+                    szText = (char*)malloc(iTextLength+1);
+                    SendMessage(hnewItem, WM_GETTEXT, iTextLength+1, (LPARAM)szText);
+                    SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)szText);
+                    SendMessage(hnewItem, WM_SETTEXT, 0, (LPARAM)"");
+                    free(szText);
+                    break;
+            }
+            break;
         case WM_PAINT:
             hdc = BeginPaint (hwnd, &ps) ;
-            GetClientRect (hwnd, &rect) ;
-            DrawText (hdc, TEXT ("Hello World"), -1, &rect,
-            DT_SINGLELINE | DT_CENTER | DT_VCENTER) ;
             EndPaint (hwnd, &ps) ;
             return 0 ;
         case WM_DESTROY:
@@ -58,4 +89,31 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
             return 0 ;
     }
     return DefWindowProc (hwnd, message, wParam, lParam) ;
+}
+
+void createWindows(HWND hwnd) {
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+    hListBox = CreateWindowExW(WS_EX_CLIENTEDGE
+            , L"LISTBOX", NULL
+            , WS_CHILD | WS_VISIBLE
+            , 7, 35, 200, 300
+            , hwnd, NULL, hInstance, NULL);
+
+    hnewItem = CreateWindow(
+            TEXT("Edit"),
+            TEXT(""),
+            WS_CHILD | WS_VISIBLE | WS_BORDER,
+            5, 5, 100, 20, hwnd,
+            (HMENU)ID_NEW_ITEM,
+            GetModuleHandle(NULL),
+            NULL);
+
+    hAddButton = CreateWindow(
+            TEXT("Button"),
+            TEXT("Add"),
+            WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+            110, 5, 50, 20, hwnd,
+            (HMENU)ID_ADD_BUTTON,
+            GetModuleHandle(NULL),
+            NULL);
 }
--- PW/lab2/resource.h
+++ PW/lab2/resource.h
@@ -5,6 +5,11 @@
 #ifndef LAB2_RESOURCE_H
 #define LAB2_RESOURCE_H
 
+#define LIST_BOX   101
+#define ID_ABOUT   102
+#define ID_EXIT    103
+#define ID_NEW_ITEM 104
+#define ID_ADD_BUTTON 105
 #define IDC_TARGET 1000
 #define IDE_TARGET 1001
 

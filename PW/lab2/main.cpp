b160077d1e3c4fff753e1fecdc12e0fe74caf40e
vitiok <lucavictor220@gmail.com>
2016-05-06 16:46:08 +0300

    Added rename control to list-box

--- PW/lab2/main.cpp
+++ PW/lab2/main.cpp
@@ -1,11 +1,10 @@
 #include <windows.h>
+#include <w32api/windowsx.h>
 #include "resource.h"
 
 
-HWND hListBox, hAddButton, hRemoveButton, hClearButton;
-HMENU hmenu;
-HWND hnewItem;
-HMENU hPopupMenu;
+HWND hListBox, hAddButton, hRemoveButton, hClearButton, hNewItem, hRenameItem;
+HMENU hPopUpMenu;
 static HWND hScrollBars[3], hLabel[3], hValue[3];
 static COLORREF crPrim[3] = { RGB (255, 0, 0), RGB (0, 255, 0), RGB (0, 0, 255) } ;
 static HBRUSH hBrush[3], hBrushStatic ;
@@ -13,8 +12,12 @@ static HBRUSH hBrush[3], hBrushStatic ;
 
 LRESULT CALLBACK WndProc (HWND, UINT, WPARAM, LPARAM) ;
 
+BOOL CALLBACK RenameDlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+
 void createWindows(HWND);
+void SetRandomColor(HWND, int*, RECT);
 static TCHAR szAppName[] = TEXT ("HelloWin") ;
+static TCHAR szRenameWindow[] = TEXT("Rename");
 
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
     HWND hwnd ;
@@ -50,6 +53,10 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine
              NULL, // window menu handle
              hInstance, // program instance handle
              NULL) ; // creation parameters
+
+    RegisterHotKey(hwnd, HK_CTRL_X, MOD_CONTROL, 0x58);
+    RegisterHotKey(hwnd, HK_CRTL_SPACE, MOD_CONTROL, VK_SPACE);
+
     ShowWindow (hwnd, iCmdShow) ;
     UpdateWindow (hwnd) ;
     while (GetMessage (&msg, NULL, 0, 0)) {
@@ -65,13 +72,14 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
     RECT rect ;
     UINT iTextLength;
     char* szText;
-    POINT pt;
-    int index, i;
-    HINSTANCE hInstance;
+    static POINT pt;
+    int index, i = 0;
+    HINSTANCE hInstance = nullptr;
     static int color[3];
-    
+
     switch (message) {
         case WM_CREATE:
+            hInstance = (HINSTANCE) ((LPCREATESTRUCT) lParam)->hInstance ;
             hMenu = LoadMenu (hInstance, szAppName) ;
             hMenu = GetSubMenu (hMenu, 0) ;
             createWindows(hwnd);
@@ -79,17 +87,17 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
         case WM_COMMAND:
             switch (LOWORD(wParam)){
                 case ID_ADD_BUTTON:
-                    iTextLength = SendMessage(hnewItem, WM_GETTEXTLENGTH, 0, 0);
+                    iTextLength = (UINT) SendMessage(hNewItem, WM_GETTEXTLENGTH, 0, 0);
                     if (iTextLength != 0) {
                         szText = (char*)malloc(iTextLength+1);
-                        SendMessage(hnewItem, WM_GETTEXT, iTextLength+1, (LPARAM)szText);
+                        SendMessage(hNewItem, WM_GETTEXT, iTextLength+1, (LPARAM)szText);
                         SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)szText);
-                        SendMessage(hnewItem, WM_SETTEXT, 0, (LPARAM)"");
+                        SendMessage(hNewItem, WM_SETTEXT, 0, (LPARAM)"");
                         free(szText);
                     }
                     break;
                 case ID_REMOVE_BUTTON:
-                    index = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+                    index = (int) SendMessage(hListBox, LB_GETCURSEL, 0, 0);
                     if (index != LB_ERR)
                         SendMessage(hListBox, LB_DELETESTRING, index, 0);
                     break;
@@ -99,13 +107,35 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
                 case IDM_ABOUT:
                     MessageBox(hwnd, TEXT("This is the coolest and best app in the world!"), MB_OK, MB_ICONEXCLAMATION) ;
                     break;
-                case IDM_GREETING:
-                    MessageBox(hwnd, TEXT("Hello World! :)"), MB_OK, MB_ICONEXCLAMATION) ;
+                case IDM_RESIZE:
+                    SendMessage(hListBox,  LB_SETITEMDATA, 0, (LPARAM) TEXT("Jora"));
+
+                    SetWindowPos(hwnd, HWND_TOP, 200, 200, 400, 500, SWP_NOOWNERZORDER);
                     break;
                 case IDM_EXIT:
                     PostQuitMessage (0) ;
                     break;
+                case IDM_DELETE:
+                    index = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+                    SendMessage(hListBox, LB_DELETESTRING, index, 0);
+                    break;
+                case IDM_RENAME:
+                    if (DialogBox (hInstance, MAKEINTRESOURCE(IDR_ABOUTBOX), hwnd, (DLGPROC)RenameDlgProc)){
+                        InvalidateRect(hwnd, NULL, TRUE);
+                    }
+                    break;
             }
+        case WM_CONTEXTMENU:
+            if((HWND)wParam == hListBox) {
+                hPopUpMenu = LoadMenu(NULL, MAKEINTRESOURCE(IDR_POPUPMENU));
+                hPopUpMenu = GetSubMenu(hPopUpMenu, 0);
+                ClientToScreen(hwnd, (LPPOINT) &pt);
+                pt.x = GET_X_LPARAM(lParam);
+                pt.y = GET_Y_LPARAM(lParam);
+                TrackPopupMenu (hPopUpMenu, TPM_RIGHTBUTTON, pt.x, pt.y, 0, hwnd, NULL);
+                DestroyMenu(hPopUpMenu);
+            }
+            break;
         case WM_HSCROLL:
             i = GetWindowLong ((HWND) lParam, GWL_ID) ;
             switch (LOWORD (wParam)) {
@@ -126,24 +156,36 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
             SetScrollPos (hScrollBars[i], SB_CTL, color[i], TRUE) ;
             hBrushStatic = CreateSolidBrush(RGB(color[0], color[1], color[2]));
             TCHAR szBuffer [40] ;
-            hdc = GetDC(hwnd);
             wsprintf (szBuffer, TEXT ("%i"), color[i]) ;
             SetWindowText (hValue[i], szBuffer) ;
             DeleteObject((HBRUSH)SetClassLongPtr(hwnd, GCLP_HBRBACKGROUND, (LONG_PTR) hBrushStatic));
             InvalidateRect (hwnd, &rect, TRUE) ;
-            ReleaseDC(hwnd, hdc);
             break;
         case WM_KEYDOWN:
             i = GetWindowLong ((HWND) lParam, GWL_ID) ;
             switch(wParam) {
                 case VK_UP :
                     SendMessage(hScrollBars[i], WM_HSCROLL, SB_LINELEFT, 0);
-                break;
+                    break;
                 case VK_DOWN :
                     SendMessage(hScrollBars[i], WM_HSCROLL, SB_LINERIGHT, 0);
-                break;
+                    break;
+                case VK_RETURN:
+                    MessageBox(hwnd, TEXT("This is the coolest and best app in the world!"), MB_OK, MB_ICONEXCLAMATION) ;
+//                    SetFocus(hNewItem);
+                    break;
             }
             return 0;
+        case WM_HOTKEY:
+            switch(wParam) {
+                case HK_CTRL_X:
+                    PostQuitMessage (0) ;
+                    break;
+                case HK_CRTL_SPACE:
+                    SetRandomColor(hwnd, color, rect);
+                    break;
+            }
+            break;
         case WM_PAINT:
             hdc = BeginPaint (hwnd, &ps) ;
             GetClientRect(hwnd, &rect);
@@ -165,16 +207,45 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
     return DefWindowProc (hwnd, message, wParam, lParam) ;
 }
 
+BOOL CALLBACK RenameDlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+    static HWND hEditControl;
+    int index, iTextLength;
+    TCHAR *szText;
+
+    switch(message) {
+        case WM_INITDIALOG:
+            hEditControl = GetDlgItem(hDlg, ID_EDIT) ;
+            SetFocus(hEditControl) ;
+            return FALSE ;
+        case WM_COMMAND:
+            switch(LOWORD(wParam)) {
+                case ID_DIALOG_RENAME:
+                    iTextLength = (UINT) SendMessage(hEditControl, WM_GETTEXTLENGTH, 0, 0) ;
+                    szText = (char*)malloc(iTextLength+1);
+                    SendMessage(hEditControl, WM_GETTEXT, (WPARAM) (iTextLength + 1), (LPARAM)szText) ;
+                    index = (int) SendMessage(hListBox, LB_GETCURSEL, 0, 0) ;
+                    SendMessage(hListBox, LB_DELETESTRING, 0, 0);
+                    SendMessage(hListBox, LB_INSERTSTRING, index, (LPARAM) szText);
+                    SendMessage(hEditControl, WM_SETTEXT, 0, (LPARAM)"");
+                    free(szText);
+                    EndDialog (hDlg, TRUE) ;
+                    return TRUE;
+            }
+    }
+    return FALSE; ;
+}
+
+
 void createWindows(HWND hwnd) {
     static TCHAR * szColorLabel[] = {(TCHAR *)("Red"), (TCHAR *)("Green"), (TCHAR *)("Blue")} ;
     HINSTANCE hInstance = GetModuleHandle(NULL);
-    hListBox = CreateWindowExW(WS_EX_CLIENTEDGE
-            , L"LISTBOX", NULL
-            , WS_CHILD | WS_VISIBLE | LBS_NOTIFY | WS_VSCROLL
-            , 7, 35, 200, 200
-            , hwnd, NULL, hInstance, NULL);
+    hListBox = CreateWindowExW(WS_EX_CLIENTEDGE,
+           L"LISTBOX", NULL,
+           WS_CHILD | WS_VISIBLE | LBS_NOTIFY | WS_VSCROLL | LBS_HASSTRINGS,
+           7, 35, 200, 200,
+           hwnd, (HMENU)IDC_LIST_BOX, hInstance, NULL);
 
-    hnewItem = CreateWindow(
+    hNewItem = CreateWindow(
             TEXT("Edit"),
             TEXT(""),
             WS_CHILD | WS_VISIBLE | WS_BORDER,
@@ -210,6 +281,16 @@ void createWindows(HWND hwnd) {
             GetModuleHandle(NULL),
             NULL);
 
+    CreateWindow(
+            TEXT("SCROLLBasc"),
+            (LPCTSTR) NULL,
+            WS_CHILD | WS_BORDER| WS_VISIBLE,
+            100, 100, 400, 400,
+            hwnd,
+            (HMENU)(ID_CHILD_WINDOW),
+            hInstance,
+            NULL);
+
     for (int i = 0; i < 3; i++) {
         hScrollBars[i] = CreateWindow (
                 TEXT ("scrollbar"), NULL,
@@ -238,5 +319,17 @@ void createWindows(HWND hwnd) {
         hBrush[i] = CreateSolidBrush (crPrim[i]) ;
 
     }
-    hBrushStatic = CreateSolidBrush (GetSysColor (COLOR_BTNHIGHLIGHT)) ;
+}
+
+void SetRandomColor(HWND hwnd, int *color, RECT rect) {
+    TCHAR szBuffer [40] ;
+    for (int i = 0; i < 3; ++i) {
+        color[i] = rand() % 255;
+        SetScrollPos (hScrollBars[i], SB_CTL, color[i], TRUE) ;
+        wsprintf (szBuffer, TEXT ("%i"), color[i]) ;
+        SetWindowText (hValue[i], szBuffer) ;
+    }
+    hBrushStatic = CreateSolidBrush(RGB(color[0], color[1], color[2])) ;
+    DeleteObject((HBRUSH)SetClassLongPtr(hwnd, GCLP_HBRBACKGROUND, (LONG_PTR) hBrushStatic)) ;
+    InvalidateRect (hwnd, &rect, TRUE) ;
 }
--- PW/lab2/resource.h
+++ PW/lab2/resource.h
@@ -12,13 +12,29 @@
 #define ID_ADD_BUTTON           105
 #define ID_REMOVE_BUTTON        106
 #define ID_CLEAR_BUTTON         107
+#define ID_RENAME_WINDOW        108
+#define ID_CHILD_WINDOW         109
 
 #define IDC_APPMENU             999
 #define IDC_TARGET              1000
 #define IDE_TARGET              1001
 // MENU ITEMS
 #define IDM_ABOUT               1002
-#define IDM_GREETING            1003
+#define IDM_RESIZE              1003
 #define IDM_EXIT                1004
+
+// HOT KEYS
+#define HK_CTRL_X               2000
+#define HK_CRTL_SPACE           2001
+
+//POPUPMENU
+#define IDR_POPUPMENU           3000
+#define IDM_RENAME              3001
+#define IDM_DELETE              3002
+
+// DIALOG BOX RENAME
+#define IDR_ABOUTBOX            4000
+#define ID_EDIT                 4001
+#define ID_DIALOG_RENAME        4002
 
 #endif //LAB2_RESOURCE_H
